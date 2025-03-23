082e1a5570b6264b5ffa58c3c6bd359472514e45
Saket Jha <saketj.dev@gmail.com>
2025-03-23 17:12:14 +0530

    Delete testAuth.js

--- testAuth.js
+++
@@ -1,9 +0,0 @@
-import { signup, login } from "./auth.js";
-
-const testAuth = async () => {
-  await signup("testuser@example.com", "password123");
-  await login("testuser@example.com", "password123");
-};
-
-
-testAuth();
